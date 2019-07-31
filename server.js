00e589a4d6597aabcb67ad69efffacb89ecd6560
admin <alex.darmos@gmail.com>
2019-07-31 16:52:37 -0400

    added cors

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 //Get request
 const app = express();
+const cors = require('cors');
 //allows us to use data in json format
-app.use(express.json());
+// app.use(express.json());
+app.use(cors());
+
 // server.js, any file with a created and configured Express app
 // app.use(express.static('./public'));
 
