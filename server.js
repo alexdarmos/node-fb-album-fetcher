d58e938aacb8a837a5c00063987afe40716c82c4
admin <alex.darmos@gmail.com>
2019-07-31 14:55:52 -0400

    updated port

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ app.use(express.json());
 const albums = require('./album');
 app.use('/get-photos', albums);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
-app.listen(port, () =>
-  console.log(`Listening on port: http://localhost:${port}.`)
-);
+app.listen(port, () => console.log(`Listening on port: ${port}`));
