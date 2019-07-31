8eeb228284f3780f7a730f51acfc4adbce88e4a4
admin <alex.darmos@gmail.com>
2019-07-31 14:20:27 -0400

    initialized

---
+++ album.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const request = require('request-promise');
+require('dotenv').config();
+let ALBUM_ID;
+let ACCESS_TOKEN;
+
+if (process.env.NODE_ENV !== 'production') {
+  ALBUM_ID = process.env.NODE_APP_ALBUM_ID;
+  ACCESS_TOKEN = process.env.NODE_APP_ACCESS_TOKEN;
+
+  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+  ALBUM_ID = process.env.ALBUM_ID;
+  ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+}
+
+//lets you have nested routes as modules
+const albums = express.Router();
+// const questionData = require('./questionData');
+
+albums.get('/', (req, res) => {
+  console.log(`get fb data`);
+
+  const options = {
+    method: 'GET',
+    url: `https://graph.facebook.com/${ALBUM_ID}/?fields=photos{images}&access_token=${ACCESS_TOKEN}`,
+    dataType: 'json'
+  };
+
+  request(options).then(response => {
+    const parsedData = JSON.parse(response);
+    let photoGallery = [];
+    // console.log(parsedData.photos.data[0].images[0].source);
+
+    let dataObj = parsedData.photos.data[0].images;
+    // console.log(dataObj);
+    dataObj.forEach((item, index) => {
+      photoGallery.push(item.source);
+    });
+    res.send(photoGallery);
+  });
+});
+
+module.exports = albums;
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+//Get request
+const app = express();
+//allows us to use data in json format
+app.use(express.json());
+// server.js, any file with a created and configured Express app
+// app.use(express.static('./public'));
+
+const albums = require('./album');
+app.use('/get-photos', albums);
+
+const port = 5000;
+
+app.listen(port, () =>
+  console.log(`Listening on port: http://localhost:${port}.`)
+);
